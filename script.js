bbe2d3a9be700b9e18e047774eb1a3940d33a2d8
Manish  meena <49023276+manishmeena022@users.noreply.github.com>
2023-08-28 14:21:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,72 +1,72 @@
-
-// creating variables 
+// Creating variables
 let hr = 0;
 let min = 0;
 let sec = 0;
 let ms = 0;
 let intervalId;
 
-//Event handling
+// Event handling
 const stopwatchEl = document.querySelector('.stopwatch');
-const startBtn = document.querySelector('.start-btn').addEventListener('click',startstopwatch);
-const pauseBtn = document.querySelector('.pause-btn').addEventListener('click',pausestopwatch);
-const resetBtn = document.querySelector('.reset-btn').addEventListener('click',resetstopwatch);
+const startBtn = document.querySelector('.start-btn');
+const pauseBtn = document.querySelector('.pause-btn');
+const resetBtn = document.querySelector('.reset-btn');
 
+startBtn.addEventListener('click', startStopwatch);
+pauseBtn.addEventListener('click', pauseStopwatch);
+resetBtn.addEventListener('click', resetStopwatch);
 
-// to star the stopwatch
-function startstopwatch(){
-    intervalId = setInterval(updateStopwatch ,10);
-    startBtn.disable = true;
-    pauseBtn.disable = false;
+// To start the stopwatch
+function startStopwatch() {
+    intervalId = setInterval(updateStopwatch, 10);
+    startBtn.disabled = true; // Disable the start button when running
 }
 
-
-
-// to update the stopwatch after every millisecond
+// To update the stopwatch after every millisecond
 function updateStopwatch() {
     ms += 1;
-    if(ms === 100){
+    if (ms === 100) {
         sec++;
         ms = 0;
     }
-    if(sec === 60){
+    if (sec === 60) {
         min++;
-        sec =0;
+        sec = 0;
     }
-    if(min === 60){
-        hr++ ;
+    if (min === 60) {
+        hr++;
         min = 0;
     }
     updateStopwatchDisplay();
 }
 
-// update digital timer of the stopwatch 
+// Update digital timer of the stopwatch
 function updateStopwatchDisplay() {
     stopwatchEl.querySelector('#hr').textContent = padTime(hr);
     stopwatchEl.querySelector('#min').textContent = padTime(min);
     stopwatchEl.querySelector('#sec').textContent = padTime(sec);
     stopwatchEl.querySelector('#ms').textContent = padTime(ms, 2);
 }
-function padTime(time,padding = 2){
-    return time.toString().padStart(padding,'0');
+
+function padTime(time, padding = 2) {
+    return time.toString().padStart(padding, '0');
 }
 
-
-// to pasue the stop watch
-function pausestopwatch () {
+// To pause the stopwatch
+function pauseStopwatch() {
     clearInterval(intervalId);
-    startBtn.disable = false;
-    pauseBtn.disable = true;
+    startBtn.disabled = false; // Re-enable the start button
 }
 
-// to reset the stopwatch
-function resetstopwatch() {
+// To reset the stopwatch
+function resetStopwatch() {
     clearInterval(intervalId);
     hr = 0;
     min = 0;
-    sec =0;
+    sec = 0;
     ms = 0;
     updateStopwatchDisplay();
-    startBtn.disable = false;
-    pauseBtn.disable = true;
+    startBtn.disabled = false; // Re-enable the start button
 }
+
+// Initially, enable the start button and update display
+updateStopwatchDisplay();
